87db9d643b7b7da9ed4cd480ffb5cbcbaa03397d
Mendess2526 <pedro.mendes.26@gmail.com>
2018-01-29 00:26:37 +0000

    Changes module name.

--- src/com/github/mendess2526/memnarch/Command.java
+++ src/com/github/mendess2526/memnarch/Command.java
@@ -1,6 +1,5 @@
 package com.github.mendess2526.memnarch;
 
-import com.sun.istack.internal.NotNull;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
 import sx.blah.discord.handle.obj.Permissions;
 
@@ -10,6 +9,5 @@ import java.util.Set;
 public interface Command {
     void runCommand(MessageReceivedEvent event, List<String> args);
     String getCommandGroup();
-    @NotNull
     Set<Permissions> getPermissions();
 }
